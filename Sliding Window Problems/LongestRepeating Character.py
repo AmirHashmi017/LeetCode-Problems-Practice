281fe7f3b6282f2a6820a0ce5de05d97e6062b7d
Amir Hashmi <amirhashmi017@gmail.com>
2025-01-13 19:18:53 +0500

    Sliding Window Problems

--- Sliding Window Problems/LongestRepeating Character.py
+++ Sliding Window Problems/LongestRepeating Character.py
@@ -1,30 +1,17 @@
 def characterReplacement( s, k):    
+    hashmap={}
     left=0
-    right=1
     maxlength=0
-    while(right<len(s)):
-        shifted=0
-        length=1
-        print(s[left], s[right])
-        if(s[left]==s[right]):
-            print(s[left], s[right])
-            length+=1
-            print(length)
-        elif(shifted<k):
-            print(s[left], s[right])
-            length+=1
-            shifted+=1
-            print(length)
-        elif(shifted>=k):
-            print(s[left], s[right])
-            print("Yes")
-            left=right
-            maxlength=max(maxlength,length)
-            length=1
-            shifted=0
-        right+=1
-    maxlength=max(maxlength,length)
+    for right in range(len(s)):
+        if(hashmap.get(s[right])!=None):
+            hashmap[s[right]]=1+hashmap.get(s[right])
+        else:
+            hashmap[s[right]]=1
+        while((right-left+1)-max(hashmap.values())>k):
+            hashmap[s[left]]-=1
+            left+=1
+        maxlength=max(maxlength,right-left+1)
     return maxlength
     
-
-print(characterReplacement("ABAB",2))
+s="KRSCDCSONAJNHLBMDQGIFCPEKPOHQIHLTDIQGEKLRLCQNBOHNDQGHJPNDQPERNFSSSRDEQLFPCCCARFMDLHADJADAGNNSBNCJQOF"
+print(characterReplacement(s,4))
