37e9703ddfdcfdf62388f6caf6cb491f4184e191
Amir Hashmi <amirhashmi017@gmail.com>
2025-01-17 17:38:30 +0500

    Sliding Window Problems

--- Sliding Window Problems/SlidingWindowMaximum.py
+++ Sliding Window Problems/SlidingWindowMaximum.py
@@ -1,4 +1,5 @@
 import heapq
+import collections
 #Solution with Heapq in O(nlogn)
 def maxSlidingWindowHeapq(nums, k):
     if(len(nums)==0):
@@ -18,3 +19,23 @@ def maxSlidingWindowHeapq(nums, k):
     return maximumwindow
 
 print(maxSlidingWindowHeapq([1,3,-1,-3,5,3,6,7],3))
+
+#Solution with Dequeue in O(n)
+def maxSlidingWindowDequeue(nums, k):
+    maximumwindow=[]
+    q=collections.deque()
+    left=0
+    right=0
+    while(right<len(nums)):
+        while(q and nums[q[-1]]<nums[right]):
+            q.pop()
+        q.append(right)
+        if(q[0]<left):
+            q.popleft()
+        if((right-left+1)>=k):
+            maximumwindow.append(nums[q[0]])
+            left+=1           
+        right+=1
+    return maximumwindow
+
+print(maxSlidingWindowDequeue([1,3,-1,-3,5,3,6,7],3))
