a4fa1dbf68fd9d67f3d459f89286969badf27d22
Amir Hashmi <amirhashmi017@gmail.com>
2025-01-17 16:56:38 +0500

    Sliding Window Problems

---
+++ Sliding Window Problems/SlidingWindowMaximum.py
@@ -0,0 +1,19 @@
+import heapq
+def maxSlidingWindow(nums, k):
+    if(len(nums)==0):
+        return []
+    heap=[]
+    maximumwindow=[]
+    for i in range(0,k,1):
+        heapq.heappush(heap,(-nums[i],i))
+    maximumwindow.append(-heap[0][0])
+    left=1
+    for j in range(k,len(nums),1):
+        heapq.heappush(heap,(-nums[j],j))
+        while(heap[0][1]<left):
+            heapq.heappop(heap)
+        maximumwindow.append(-heap[0][0])
+        left+=1
+    return maximumwindow
+
+print(maxSlidingWindow([1,3,-1,-3,5,3,6,7],3))
