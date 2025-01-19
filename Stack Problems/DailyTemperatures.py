3d0fc95dfc3f6f6d2796e8f7599261d9ab81ccb1
Amir Hashmi <amirhashmi017@gmail.com>
2025-01-19 17:23:43 +0500

    Stack Problems

--- Stack Problems/DailyTemperatures.py
+++ Stack Problems/DailyTemperatures.py
@@ -1,27 +1,12 @@
 def dailyTemperatures(temperatures):
+    stack=[]
     output=[0]*len(temperatures)
-    for i in range(len(temperatures)-2,-1,-1):
+    for i in range(len(temperatures)):
         count=0
-        days=0
-        j=i+1
-        while(j<len(temperatures)):
-            print(j)
-            print(temperatures[j])
-            if(temperatures[i]<temperatures[j]):
-                print(temperatures[i])
-                print(temperatures[j])
-                print(count)
-                days=count
-                days+=1
-                break
-            elif(output[j]>0):
-                print(output[j])
-                count+=output[j]
-                j+=output[j]
-            else:
-                break
-            print(j)
-        output[i]=days
+        while(stack and stack[-1][0]<temperatures[i]):
+            value,index=stack.pop()
+            output[index]=i-index
+        stack.append((temperatures[i],i))
     return output
 
 print(dailyTemperatures([73,74,75,71,69,72,76,73]))
