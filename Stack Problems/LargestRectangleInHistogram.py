f92b79b771150df3407792905c5b33bb59312e9c
Amir Hashmi <amirhashmi017@gmail.com>
2025-01-21 21:17:14 +0500

    Stack Problems

--- Stack Problems/LargestRectangleInHistogram.py
+++ Stack Problems/LargestRectangleInHistogram.py
@@ -1,37 +1,22 @@
 def largestRectangleArea(heights):
     stack=[]
-    count=1
-    minheight=heights[0]
-    left=0
-    right=0
-    while(right<len(heights) and left<len(heights)):
-        print(stack)
-        if(count==1):
-            stack.append(heights[right]*count)
-            print(stack)
-            count+=1
-            right+=1
-        else:
-            print(minheight)
-            minheight=min(minheight,heights[right])
-            print(minheight)
-            area=(minheight*count)
-            print(count)
-            print(area)
-            if(area>stack[-1]):
-                stack.append(area)
-                right+=1
-                count+=1
-            else:
-                count=1
-                left+=1
-                right=left
-                minheight=heights[right]
-        if(right==len(heights) and left<len(heights)-1):
-            count=1
-            left+=1
-            right=left
-            minheight=heights[right]
-    return max(stack)
+    maxarea=0
+    for i in range(len(heights)):
+        width=0
+        while(stack and heights[i]<stack[-1][0]):
+            height,index=stack[-1]
+            width=(i-index)
+            maxarea=max(maxarea,height*width)
+            stack.pop()
+        
+        stack.append((heights[i],i-width))
+    
+    while(stack):
+        height,width=stack[-1]
+        area=height*(len(heights)-width)
+        maxarea=max(maxarea,area)
+        stack.pop()
+    
+    return maxarea
 
-print(largestRectangleArea([1,1]))
+print(largestRectangleArea([2,1,5,6,2,3]))
