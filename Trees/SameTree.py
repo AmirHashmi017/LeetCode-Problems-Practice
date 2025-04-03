74f0549b1aed5bb0e2eca689fdd569199d5c49fb
Amir Hashmi <amirhashmi017@gmail.com>
2025-04-03 17:57:24 +0500

    Balanced Binary Tree

---
+++ Trees/SameTree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+       
+
+        queuep=[p]
+        queueq=[q]
+
+        while(queuep and queueq):
+            nodep=queuep.pop(0)
+            nodeq=queueq.pop(0)
+            if(not nodep and not nodeq):
+                continue
+            elif(not nodep or not nodeq or nodep.val!=nodeq.val):
+                return False
+                
+            queuep.append(nodep.left)
+            queuep.append(nodep.right)
+            queueq.append(nodeq.left)
+            queueq.append(nodeq.right)
+
+        
+        if(not queuep and not queueq):
+            return True
+        return False
+        
