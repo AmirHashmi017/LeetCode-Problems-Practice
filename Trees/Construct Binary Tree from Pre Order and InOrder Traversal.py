2d098f3c1b1adac432e0072c33aed1f84904df57
Amir Hashmi <amirhashmi017@gmail.com>
2025-04-05 13:46:14 +0500

    Construct Binary Tree from Pre Order and InOrder Traversal

---
+++ Trees/Construct Binary Tree from Pre Order and InOrder Traversal.py
@@ -0,0 +1,46 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def __init__(self):
+        self.hashmapinorder={}
+        self.idx=0
+    def buildTree(self, preorder, inorder):
+        """
+        :type preorder: List[int]
+        :type inorder: List[int]
+        :rtype: Optional[TreeNode]
+        """
+        for i in range(len(inorder)):
+            self.hashmapinorder[inorder[i]]=i
+        
+        def DFSRecursive(left,right):
+            if(left>right):
+                return None
+
+            rootval=preorder[self.idx]
+            self.idx+=1
+
+            root=TreeNode(rootval)
+            mid=self.hashmapinorder[rootval]
+            
+
+            root.left=DFSRecursive(left,mid-1)
+            root.right=DFSRecursive(mid+1,right)
+            return root
+        
+        return DFSRecursive(0,len(inorder)-1)
+    
+    
+
+        
+
+        
+        
+
+
+            
+        
